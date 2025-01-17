62de5ad1e99178eed34c8687faad304833bc13a1
sahana-source <72065407+sahana-source@users.noreply.github.com>
2025-01-17 12:06:47 +0000

    hjfhgj

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -12,24 +12,8 @@ public class CalculatorTest {
     }
 
     @Test
-    public void testSubtract() {
-        assertEquals(1, calculator.subtract(3, 2), "3 - 2 should equal 1");
-        assertEquals(-5, calculator.subtract(-2, 3), "-2 - 3 should equal -5");
-    }
-
-    @Test
     public void testMultiply() {
         assertEquals(6, calculator.multiply(2, 3), "2 * 3 should equal 6");
         assertEquals(0, calculator.multiply(0, 5), "0 * 5 should equal 0");
-    }
-
-    @Test
-    public void testDivide() {
-        assertEquals(2, calculator.divide(6, 3), "6 / 3 should equal 2");
-        assertEquals(-2, calculator.divide(-6, 3), "-6 / 3 should equal -2");
-
-        // Test division by zero
-        Exception exception = assertThrows(ArithmeticException.class, () -> calculator.divide(1, 0));
-        assertEquals("Division by zero is not allowed.", exception.getMessage());
     }
 }
